624a51cd0510047483630ad6950119a3b8223cb4
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-04-29 00:35:21 +0300

    Added docstrings to the databases

--- database.py
+++ database.py
@@ -6,12 +6,15 @@ from typing import Optional
 
 @dataclass
 class Guild:
+    """Stores the configuration of a specified guild"""
+
     moderator_role: Optional[int] = None
     cases_channel: Optional[int] = None
     duplication_webhook: Optional[int] = None
 
     @classmethod
     def from_dict(cls, dictionary: Optional[dict]):
+        """Create a config object from a dictionary representation"""
         if dictionary:
             return cls(
                 moderator_role=int(dictionary["moderator_role"])
@@ -26,7 +29,8 @@ class Guild:
             )
         return cls()
 
-    def to_dict(self):
+    def to_dict(self) -> dict[str, Optional[str]]:
+        """Generate a dictionary representation of the configuration"""
         return {
             "moderator_role": str(self.moderator_role) if self.moderator_role else None,
             "cases_channel": str(self.cases_channel) if self.cases_channel else None,
@@ -37,10 +41,14 @@ class Guild:
 
 
 class Database(ABC):
+    """An abstract representation of the database"""
+
     @abstractmethod
     def get_guild(self, guild_id: int) -> Guild:
+        """Retrieve the configuration for a given guild"""
         pass
 
     @abstractmethod
     def set_guild(self, guild_id: int, guild: Guild) -> None:
+        """Save the configuration of a guild"""
         pass
--- firebase_db.py
+++ firebase_db.py
@@ -7,17 +7,22 @@ from database import Guild
 
 
 class FirestoreDatabase(Database):
+    """Google Firestore implementation of the database"""
+
     def __init__(self, credentials_file="credentials.json"):
+        """Initialize the firestore database"""
         self.cred = credentials.Certificate(credentials_file)
         firebase_admin.initialize_app(self.cred)
 
         self.db = firestore.client()
 
     def get_guild(self, guild_id: int) -> Guild:
+        """Retrieve the configuration for a given guild"""
         doc_ref = self.db.collection("guilds").document(str(guild_id))
         doc = doc_ref.get()
         return Guild.from_dict(doc.to_dict())
 
-    def set_guild(self, guild_id, guild: Guild):
+    def set_guild(self, guild_id, guild: Guild) -> None:
+        """Save the configuration of a guild"""
         doc_ref = self.db.collection("guilds").document(str(guild_id))
         doc_ref.set(guild.to_dict())
